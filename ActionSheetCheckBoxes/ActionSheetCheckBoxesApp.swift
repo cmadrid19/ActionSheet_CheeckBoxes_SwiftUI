98b591b52b919d5e53118b35008613a32238a5c0
Max <maxim.macari.dam@gmail.com>
2020-11-08 18:25:25 +0100

    Initial Commit

---
+++ ActionSheetCheckBoxes/ActionSheetCheckBoxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionSheetCheckBoxesApp.swift
+//  ActionSheetCheckBoxes
+//
+//  Created by Maxim Macari on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ActionSheetCheckBoxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActionSheetCheckBoxes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActionSheetCheckBoxes
+//
+//  Created by Maxim Macari on 08/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
